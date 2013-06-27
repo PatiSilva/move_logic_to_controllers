b2cfa1028ebb1eb96dd600bbf1d85c8b8fb7c850
MakerSquare Student <student@lab-4.att.net>
2013-06-27 17:07:01 -0500

    Logic to Controllers Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,22 @@ class UsersController < ApplicationController
       format.html # index.html.erb
       format.json { render json: @users }
     end
+
   end
+
+  def create
+  @product = Product.new(params[:product])
+
+  respond_to do |format|
+    if @product.save
+      format.html { render action: "create" }
+      format.json { render json: @product }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
+    end
+  end
+end
 
   # GET /users/1
   # GET /users/1.json
